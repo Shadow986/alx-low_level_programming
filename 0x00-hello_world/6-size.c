950d9ff2df74ccccd9929dd6f12b332ed65f17e5
Shadow986 <archie986jr@gmail.com>
2023-08-10 02:07:23 -0700

    Archie

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program that prints the size of various computer types
+ * Return: Always 0 (succss)
+ */
+int main(void)
+{
+	char x;
+	int y;
+	long int w;
+	long long int u;
+	float z;
+printf("Size of a char: %lu byte(s)\n", sizeof(x));
+printf("size of int: %lu byte(s)\n", sizeof(y));
+printf("size of long int: %lu byte(s)\n"' sizeof(w));
+printf("size of long long int: %lu byte(s)\n", sizeof(u));
+printf("size of fload: %lu byte(s)\n", sizeof(z))
+return (0);
+}
