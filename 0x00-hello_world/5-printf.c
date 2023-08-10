cb3025f54c01a1d49c4ad9b7384b46b2b3c5ac69
Shadow986 <archie986jr@gmail.com>
2023-08-10 00:09:12 -0700

    im getting good

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point of a program prints using the printf function no puts
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
