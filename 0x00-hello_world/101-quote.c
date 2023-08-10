22cdef829da7a92d817ed4a090d35ff2b30fa874
Shadow986 <archie986jr@gmail.com>
2023-08-10 04:02:30 -0700

    finally done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a standard error
+ * main - c program that prints a standard error
  * Return: 1 (success)
  */
 int main(void)
