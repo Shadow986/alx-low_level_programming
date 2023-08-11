c78a74ef3914ea1eedc4646a1158cb439a3b7dfd
Shadow986 <archie986jr@gmail.com>
2023-08-11 03:08:23 -0700

    that was tough

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for program that
+ * prints the alphabates in lowercase
+ * Return: Everythime 0 (success)
+ */
+
+int main(void)
+{
+	char x;
+
+	x = 'b';
+	while
+		(x <= 'z') {
+			putchar(x);
+			x++;
+		}
+	putchar('\n';
+	return (0);
+}
