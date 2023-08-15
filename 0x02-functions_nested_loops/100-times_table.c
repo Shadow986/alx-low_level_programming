7fa80e9eb7cd9dba7a13a6686e4c862e92eb113d
Shadow986 <archie986jr@gmail.com>
2023-08-15 13:41:38 -0700

    yes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,36 +12,30 @@ void print_times_table(int b)
 	int y;
 	int z;
 
-	if (b >= 0 && b <= 15)
-	{
+	if (b >= 0 && b <= 15){
 		for (x = 0; x <= b; x++)
 		{
-			for (y = 0; y <= b; y++)
-			{
+			for (y = 0; y <= b; y++){
 				z = y * x;
 
-				if (y == 0)
-				{
+				if (y == 0){
 					_putchar(z + '0');
 				}
-				else if (z < 10 && y != 0)
-				{
+				else if (z < 10 && y != 0){
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(z + '0');
 				}
-				else if (z >= 10 && z < 100)
-				{
+				else if (z >= 10 && z < 100){
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((z / 10) + '0');
 					_putchar((z % 10) + '0');
 				}
-				else if (z >= 100)
-				{
+				else if (z >= 100){
 					_putchar(',');
 					_putchar(' ');
 					_putchar((z / 100) + '0');
