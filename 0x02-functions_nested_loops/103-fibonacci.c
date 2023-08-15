9b26b4ec79c82dc0c157cb594d187cecb58846aa
Shadow986 <archie986jr@gmail.com>
2023-08-15 15:27:03 -0700

    az

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point finds and prints
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	unsigned long int y, z, next, sum;
+
+	y = 1;
+	z = 2;
+	sum = 0;
+
+	for (x =1; x <= 33; x++)
+	{
+		if (y < 4000000 && (y % 2) == 0)
+		{
+			sum = sum + y;
+		}
+		next = y + z;
+		y = z;
+		z = next;
+	}
+	printf("%lu\n", sum);
+	return(0);
+}
