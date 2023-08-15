68b2d51b7c2d56e99ba6886c10ce8066aa018b52
Shadow986 <archie986jr@gmail.com>
2023-08-15 03:48:53 -0700

    yes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,39 @@
 #include "main.h"
 
 /**
- * print_alphabet - print all alphabet in lowercase
- * _putchar Print a single character
+ * _putchar - Print a single character
  * @c: The character to be printed
  */
-
-void print_alphabet(void)
-
+void _putchar(char c)
 {
+    // Assuming you have a method 'output_char' to print a character
+    asm volatile (
+        "mov $0x02, %%eax\n"  // syscall number for write
+        "mov $0x01, %%ebx\n"  // file descriptor 1 (stdout)
+        "mov %[ch], %%ecx\n"  // character to print
+        "mov $0x01, %%edx\n"  // length of data
+        "int $0x80\n"
+        :
+        : [ch] "r" (c)
+        : "eax", "ebx", "ecx", "edx"
+    );
+}
 
-	char character;
+/**
+ * print_alphabet - Print all alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+    char character;
 
-	for (character = 'a'; character <= 'z'; character++)
+    for (character = 'a'; character <= 'z'; character++)
+        _putchar(character);
 
-		_putchar(character);
+    _putchar('\n');
+}
 
-
-	_putchar('\n');
-
+int main(void)
+{
+    print_alphabet();
+    return (0);
 }
