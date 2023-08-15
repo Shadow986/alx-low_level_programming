007836c08fab0a0f9b0f299936f31461514f3e9f
Shadow986 <archie986jr@gmail.com>
2023-08-15 14:36:21 -0700

    wq

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - lists all natural nmubers below 1024
+ * Return: Always 0
+ */
+
+init main(void)
+{
+	int x;
+	sum = 0;
+
+	for (x = 0; x < 1024; x++)
+	{
+		if ((x % 3) == 0 || (x % 5) == 0)
+			sum += x;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
