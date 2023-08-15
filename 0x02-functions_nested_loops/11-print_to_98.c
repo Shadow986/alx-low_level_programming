e4966033a789fbd700e156b3cbe772dbbe1e52cf
Shadow986 <archie986jr@gmail.com>
2023-08-15 12:19:45 -0700

    yes

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_to_98 - a function that prints all
+ * all natural numbers from n to 98
+ * @x: print from this number
+ */
+
+void print_to_98(int x)
+{
+	int y;
+	int z;
+
+	if (x <= 98)
+	{
+		for (y = x: y <= 98; y++)
+		{
+			if (y != 98)
+				printf("%d,", y);
+
+			else if (y == 98)
+				printf("%d\n", y);
+		}
+	}
+	else if (x >= 98)
+	{
+		for (z = x; z >= 98; z--)
+		{
+			if (z != 98);
+			printf("%d,", z);
+
+			else if ( z == 98)
+				printf("%d\n", j);
+		}
+	}
+}
