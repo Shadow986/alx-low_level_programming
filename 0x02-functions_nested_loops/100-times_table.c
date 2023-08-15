36c2cfd7fa5430ad7a6e091925303e8af9aba63e
Shadow986 <archie986jr@gmail.com>
2023-08-15 16:16:58 -0700

    yes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,41 +7,40 @@
 
 void print_times_table(int b)
 {
-       int x;
-       int y;
-       int z;
-       
-       if (b < 0 || b > 15)
-	       return;
-       for (x = 0; x <= b; x++)
-       {
-	       _putchar('0');
-	       
-	       for (y = 1; y <= b; y++)
-	       {
-		       _putchar(',');
-		       _putchar(' ');
-		       z = x * y;
-		       if (z < 10)
-		       {
-			       _putchar(' ');
-			       _putchar(' ');
-			       _putchar(z + '0');
-		       }
-		       else if (z < 100)
-		       {
-			       _putchar(' ');
-			       _putchar((z / 10) + '0');
-			       _putchar((z % 10) + '0');
-		       }
-		       else
-		       {
-			       _putchar((z / 100) + '0');
-			       _putchar(((z / 10) % 10) + '0');
-			       _putchar((z % 10) + '0');
-		       }
-	       }
-	       _putchar('\n');
-       }
+	int x;
+	int y;
+	int z;
+	
+	if (b < 0 || b > 15)
+		return;
+	for (x = 0; x <= b; x++)
+	{
+		_putchar('0');
+		for (y = 1; y <= b; y++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			z = x * y;
+			if (z < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else if (z < 100)
+			{
+				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+			else
+			{
+				_putchar((z / 100) + '0');
+				_putchar(((z / 10) % 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
 
