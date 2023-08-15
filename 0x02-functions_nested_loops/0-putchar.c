c6baeb43c93208357ad1cbd94609ac338850470a
Shadow986 <archie986jr@gmail.com>
2023-08-15 07:16:51 -0700

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entry point for a program that prints
- * the characters using the _putchar function
- * is witten by mbah
+ * main - print _putchar
+ * is written by mbah
  * Return: 0
  */
 
