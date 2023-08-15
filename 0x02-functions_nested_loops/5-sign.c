41987ff9d5feb73e677d80157f899de96a765218
Shadow986 <archie986jr@gmail.com>
2023-08-15 09:36:03 -0700

    yes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,8 +5,8 @@
  * the sign of a number
  * @x: the int to check
  * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * 0 and prints 0 if x is zero
+ * -1 and prints - if x is less than zero
  */
 
 int print_sign(int x)
