b6b2ca1e840aad8c5bb463b6c903488b0d986c0d
Shadow986 <archie986jr@gmail.com>
2023-08-17 04:00:35 -0700

    yes

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_most_numbers - a function that prints the numbers
  * from 0 to 9
- * Return: 0
+ * Return: Everytime 0
  */
 
 void print_most_numbers(void)
@@ -13,10 +13,7 @@ void print_most_numbers(void)
 	for (x = '0'; x <= '9'; x++)
 	{
 		if (!(x == '2' || x == '4'))
-			
-
 			_putchar(x);
 	}
-
 	_putchar('\n');
 }
