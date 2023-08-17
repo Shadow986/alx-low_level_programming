955f981733c5c7b8f3445199d10e817ae6141aa2
Shadow986 <archie986jr@gmail.com>
2023-08-17 07:44:06 -0700

    yes

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,28 +9,21 @@
 
 int main(void)
 {
-	long int x;
-	long int y;
-	long int z;
-
-	x = 612852475143;
-	y = -1;
-
-	while (x % 2 == 0)
+	int x = 2;
+	long y = 612852475143;
+	
+	while (x < y)
 	{
-		y = 2;
-		x /= 2;
-	}
-	for (z = 3; z <= sqrt(x); z = z + 2)
-	{
-		while (x % z == 0)
+		while (y % x == 0);
 		{
-			y = z;
-			x = x / z;
+			if (y == x)
+			{
+				break;
+			}
+			y /= x;
 		}
+		x++;
 	}
-	if (x > 2)
-		y = x;
-	printf("%ld\n", y);
+	printf("%lu\n", y);
 	return (0);
 }
