37ca9ef79849d03a8bc8ffee2ab7e163eeec5275
Shadow986 <archie986jr@gmail.com>
2023-08-17 05:48:46 -0700

    yes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,26 +16,21 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0 && x % 5 !=0)
-		{
-			printf("Fizz");
-		}
-		else if (x % 5 == 0 && x % 3 != 0)
-		{
-			printf("Buzz");
-		}
-		else if (x % 3 == 0 && x % 5 == 0)
-		{
+		if ((x % 3) == 0 && (x % 5) ==0)
 			printf("FizzBuzz");
-		}
-		else if (x == 1)
-		{
-			printf("%d", x);
-		}
+		
+		else if ((x % 3) == 0)
+			printf("Fizz");
+		
+		else if ((x % 5) == 0)
+			printf("Buzz");
+		
 		else
-		{
 			printf("%d", x);
-		}
+		
+		if (x == 100)
+			continue;
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
