233741e90e853220ea0f85fc1fb4f18daa65a43d
Shadow986 <archie986jr@gmail.com>
2023-08-21 03:54:11 -0700

    yes

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,15 +12,16 @@ void print_rev(char *s)
 	int x;
 	int y;
 
-	x = 0;
+	y = 0;
 
-	while (s[x])
+	if (*s != '\0')
 	{
-		x++;
-	}
-	for (int y = x - 1; y >= 0; y--)
-	{
-		_putchar(s[y]);
+		while (*(s + y) != '\0')
+		{
+			y++;
+		}
+		for (x = y - 1; x >= 0; x--)
+			_putchar(*(s + x));
 	}
 	_putchar('\n');
 }
