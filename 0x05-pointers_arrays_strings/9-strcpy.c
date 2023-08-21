52494f9da52db6c15a5667a2c3ea6469ca007988
Shadow986 <archie986jr@gmail.com>
2023-08-21 06:11:34 -0700

    yes

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,21 +10,15 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int x;
-	int y;
+	int x = 0;
 
-	x = 0;
-	y = 0;
-
-	while (*(src + x) != '\0')
+	while (src[x] != '\0')
 	{
+		dest[x] = src[x];
 		x++;
 	}
 
-	for (; y < x; y++)
-	{
-		dest[y] = src[y];
-	}
-	dest[1] = '\0';
+	dest[x] = '\0';
+
 	return (dest);
 }
