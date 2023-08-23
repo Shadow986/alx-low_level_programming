2174f6784bb542053e4a1d40051560585d5b4067
Shadow986 <archie986jr@gmail.com>
2023-08-23 09:34:46 -0700

    yes

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,32 @@
 #include "main.h"
 
 /**
- * leet - afunction that encodes a string into 1337
+ * leet - a function that encodes a string into 1337
  * @str: The string to be encoded
  * Return: encoded string
  */
 
 char *leet(char *str)
 {
-	char *str = y;
+	char *y = str;
 	char map[] = {'A', 'E', 'O', 'T', 'L'};
 	int cat[] = {4, 3, 0, 7, 1};
 	unsigned int x;
 
 	while (*y)
 	{
-		for (x = 0; x < sizeof(map) / sizeof(char); x++)
+		x = 0;
+		while (x < sizeof(map) / sizeof(char))
 		{
 			if (*y == map[x] || *y == map[x] + 32)
 			{
-				*y = 48 + cat[x];
+				*y = '0' + cat[x];
+				break;
 			}
+			x++;
 		}
 		y++;
 	}
 	return (str);
 }
+
