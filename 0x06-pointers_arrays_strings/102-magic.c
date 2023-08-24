68dccc6e15a0f5f2e0847190a3ad6b767f4b2040
Shadow986 <archie986jr@gmail.com>
2023-08-23 23:09:59 -0700

    yes

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,26 +2,26 @@
 
 int main(void)
 {
-	int x;
-	int y[5];
+	int n;
+	int a[5];
 	int *p;
 
-	y[2] = 1024;
-	p = &x;
+	a[2] = 1024;
+	p = &n;
 
 	/*
 	 * write your line of code here...
 	 * Remember:
-	 * not allowed to use y
-	 * not allowed to modify p
-	 * only one statement
-	 * you are not allowed to code anything else than this line of code
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
 	 */
 
 	*(p + 7 - 2) = 98;
 
 	/* ...so that this prints 98\n */
 
-	printf("y[2] = %d\n", y[2]);
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
