e9d9fb3a4ff7ca1035dba1668d80a2df69f12098
Shadow986 <Archie986jr@gmail.com>
2023-09-01 09:23:14 -0700

    yes

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a function that prints all the arg it recieves
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ * Return: Everytime 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+
+	x = 0;
+
+	for (; x < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+       	return (0);
+}
