1f35536d4d5a25792377cd4702501222c4c6a38d
Shadow986 <Archie986jr@gmail.com>
2023-09-01 09:11:18 -0700

    yes

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - a function that prints the num of args entered
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ * Return: Everytime 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv; /* Suppress unused parameter warning */
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
