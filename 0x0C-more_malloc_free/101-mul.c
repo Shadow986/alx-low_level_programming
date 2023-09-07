0e65b52abc862d95dd695e33238c6576d36dacfa
Shadow986 <Archie986jr@gmail.com>
2023-09-07 05:10:28 -0700

    yes

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,39 +1,102 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+#define ERROR_MESSAGE "Error"
+
+/**
+ * is_digit - a function that checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+	int x;
+
+	x = 0;
+
+	while (s[x])
+	{
+		if (s[x] < '0' || s[x] > '9')
+			return (0);
+		x++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: the string to evaluate
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	return (x);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("%s\n", ERROR_MESSAGE);
+	exit(98);
+}
 
 /**
  * main - a function that multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success, 98 on error
+ * @argc:the  number of arguments
+ * @argv: the array of arguments
+ * Return: always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-	int num1, num2, product;
+	char *s1, *s2;
+	int len1, len2, len, x, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (x = 0; x <= len1 + len2; x++)
+		result[x] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		printf("Error\n");
-		exit(98);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	for (int x = 0; argv[1][x]; x++)
-		if (argv[1][x] < '0' || argv[1][x] > '9')
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	for (int x = 0; argv[2][x]; x++)
-		if (argv[2][x] < '0' || argv[2][x] > '9')
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	product = num1 * num2;
-
-	printf("%d\n", product);
+	for (x = 0; x < len - 1; x++)
+	{
+		if (result[x])
+			a = 1;
+		if (a)
+			_putchar(result[x] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
