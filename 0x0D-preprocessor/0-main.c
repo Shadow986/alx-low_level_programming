48cbd00cc47a8aa0c4b5a719a951e52509b45c59
Shadow986 <Archie986jr@gmail.com>
2023-09-11 03:47:20 -0700

    yes

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,22 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 10
+
+#endif
+
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int s;
+
+	s = 98 + SIZE;
+	printf("%d\n", s);
+	return (0);
+}
