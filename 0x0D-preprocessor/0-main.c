205226a18e7b2aefab91a1e082fdce6301b50f01
Shadow986 <Archie986jr@gmail.com>
2023-09-11 04:01:44 -0700

    yes

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -4,19 +4,3 @@
 #define SIZE 10
 
 #endif
-
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int s;
-
-	s = 98 + SIZE;
-	printf("%d\n", s);
-	return (0);
-}
