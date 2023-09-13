f0cae3001a0bde9089990099377cfa7db706a37b
Shadow986 <Archie986jr@gmail.com>
2023-09-13 07:58:58 -0700

    yes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,8 +4,8 @@
 /**
  * print_name - This function takes a name (a string)
  * and a function pointer as arguments.
- * @name_to_print: The name to be printed.
- * @print_function: A pointer to the function that will print the name.
+ * @name: The name to be printed.
+ * @f: A pointer to the function that will print the name.
  * Return: NULL
  **/
 
@@ -13,6 +13,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-
 	f(name);
 }
