ecd430e3c99a216fc2c527359fe7dcbb9aad5276
Shadow986 <Archie986jr@gmail.com>
2023-09-14 04:44:51 -0700

    yes

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,45 +3,48 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - Prints anything.
- * @format: A list of types of arguments passed to the function.
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int x = 0;
-	char *str;
-	char current_char;
+	int x = 0;
+	char *str, *sep = "";
 
-	va_start(args, format);
-	while (format && format[x])
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
 	{
-		current_char = format[x];
-
-		if (current_char == 'c')
+		while (format[x])
 		{
-			printf("%c", va_arg(args, int));
+			switch (format[x])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'x':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					x++;
+					continue;
+			}
+			sep = ", ";
+			x++;
 		}
-		else if (current_char == 'x')
-		{
-			printf("%d", va_arg(args, int));
-		}
-		else if (current_char == 'f')
-		{
-			printf("%f", va_arg(args, double));
-		}
-		else if (current_char == 's')
-		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s", str);
-		}
-		x++;
-		if (format[x] && (current_char == 'c' || current_char == 'x' ||
-					current_char == 'f' || current_char == 's'))
-			printf(", ");
 	}
-	va_end(args);
+
 	printf("\n");
+	va_end(list);
 }
