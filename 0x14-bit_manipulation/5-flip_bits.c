9ef7b70e730b0bd48c23e4f873afd250e567985e
Shadow986 <archie986jr@gmail.com>
2023-09-28 04:32:30 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,12 +5,14 @@
  * you would need to flip to get from one number to another.
  * @n: the first number.
  * @m: the second number.
- * Return: the number of bits you would need to flip to get from one number to another.
+ * Return: the number of bits you would need to
+ * flip to get from one number to another.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int xor = n ^ m;
 	unsigned int count = 0;
+
 	while (xor)
 	{
 		count += xor & 1;
