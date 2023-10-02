8f9d1d960841cc855d4ca204e5a46d3c1b0558a9
Shadow986 <archie986jr@gmail.com>
2023-10-02 03:41:12 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,47 +3,42 @@
 /**
  * read_textfile - a function that reads a
  * text file and prints it to the POSIX standard output
- * @filename: The name of the file to read
- * @letters: The number of letters to read and print
- * Return: The actual number of letters read and printed, or 0 on failure
+ * @filename: pointer to the name of the file
+ * @letters: number of letters to read and print
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int x;
+	ssize_t read_count, write_count;
+	char *buffer;
+
 	if (filename == NULL)
 		return (0);
-
-	FILE *file = fopen(filename, "r");
-
-	if (file == NULL)
+	x = open(filename, O_RDONLY);
+	if (x == -1)
 		return (0);
-
-	char *buffer = malloc(sizeof(char) * (letters + 1));
-
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		fclose(file);
+		close(x);
 		return (0);
 	}
-	ssize_t bytesRead = fread(buffer, sizeof(char), letters, file);
-
-	if (bytesRead <= 0)
+	read_count = read(x, buffer, letters);
+	if (read_count == -1)
 	{
-		fclose(file);
 		free(buffer);
+		close(x);
 		return (0);
 	}
-
-	ssize_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
-
-	if (bytesWritten != bytesRead)
+	write_count = write(STDOUT_FILENO, buffer, read_count);
+	if (write_count == -1 || write_count != read_count)
 	{
-		fclose(file);
 		free(buffer);
+		close(x);
 		return (0);
 	}
-
-	fclose(file);
 	free(buffer);
-
-	return (bytesRead);
+	close(x);
+	return (write_count);
 }
